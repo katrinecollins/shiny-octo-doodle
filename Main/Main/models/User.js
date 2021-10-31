f9e1f22b0fa9bc5824288b9fce565c9976422df4
Katrine Collins <katrine.collins1@gmail.com>
2021-10-31 17:40:42 -0400

    fc

--- Main/Main/models/User.js
+++ Main/Main/models/User.js
@@ -1,6 +1,4 @@
-
-
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
   {
@@ -14,18 +12,18 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/.+@.+\..+/, "This must be an email address!"],
+      match: [/.+@.+\..+/, 'Must match an email address!'],
     },
     thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: "Thought",
+        ref: 'Thought',
       },
     ],
     friends: [
       {
         type: Schema.Types.ObjectId,
-        ref: "User",
+        ref: 'User',
       },
     ],
   },
@@ -37,10 +35,10 @@ const userSchema = new Schema(
   }
 );
 
-userSchema.virtual("friendCount").get(function () {
+userSchema.virtual('friendCount').get(function () {
   return this.friends.length;
 });
 
-const User = model("User", userSchema);
+const User = model('User', userSchema);
 
 module.exports = User;
